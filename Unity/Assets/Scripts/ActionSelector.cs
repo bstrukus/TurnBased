a16160836b3c70c292f51d52dba9f8be7a04e6f0
Ben Strukus <lokonity@gmail.com>
2019-05-25 07:19:44 -0700

    Very basic UI with my own assets in there. Need to redo input and have different interpreters for anything that responds to input.

---
+++ Unity/Assets/Scripts/ActionSelector.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Ben's TurnBased Strategy Game
+ */
+
+using UnityEngine;
+
+namespace Core
+{
+    public class ActionSelector : MonoBehaviour
+    {
+        [SerializeField]
+        private RectTransform rectTransform = null;
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
